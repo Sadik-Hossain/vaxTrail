83a8bf04ba1026cff3f4123d8245a77fabd459d9
sadik <po480373@gmai.com>
2023-03-21 12:31:33 +0600

    completed code

--- vaxTrail.js
+++ vaxTrail.js
@@ -5,9 +5,25 @@ const vaxTrail = (arr) => {
   let D = [];
 
   for (obj of arr) {
-    console.log(obj);
+    if (obj.temperature >= 100) {
+      D.push(obj);
+    } else {
+      if (obj.age >= 20 && obj.age <= 30) {
+        A.push(obj);
+      }
+      if (obj.age >= 31 && obj.age <= 40) {
+        B.push(obj);
+      }
+      if (obj.age >= 41 && obj.age <= 50) {
+        C.push(obj);
+      }
+    }
   }
-
+  // Sort each array by even ages
+  A.sort((a, b) => (a.age % 2) - (b.age % 2));
+  B.sort((a, b) => (a.age % 2) - (b.age % 2));
+  C.sort((a, b) => (a.age % 2) - (b.age % 2));
+  D.sort((a, b) => (a.age % 2) - (b.age % 2));
   return {
     A,
     B,
@@ -16,9 +32,15 @@ const vaxTrail = (arr) => {
   };
 };
 
-console.log(
-  vaxTrail([
-    { name: "sunil", age: 21, temperature: 98 },
-    { name: "Biplap", age: 22, temperature: 98 },
-  ])
-);
+const people = [
+  { name: "Biplap", age: 22, temperature: 98 },
+  { name: "sunil", age: 21, temperature: 98 },
+  { name: "Kabir", age: 36, temperature: 99 },
+  { name: "Rahul", age: 37, temperature: 99 },
+  { name: "Paul", age: 42, temperature: 98 },
+  { name: "Kat", age: 41, temperature: 98 },
+  { name: "Nayem", age: 50, temperature: 100 },
+  { name: "Sabnaj", age: 51, temperature: 101 },
+];
+
+console.log(vaxTrail(people));
